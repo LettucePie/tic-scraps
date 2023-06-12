3088ae0dd5e3d053e75a77efd3aff56f0db8f9e8
Matthew LettucePie Leemhuis <thelettucepie@gmail.com>
2023-06-12 13:48:11 -0400

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+This is just a folder for examples and experiments
+to learn and reference from.
+
+~LettucePie
---
+++ byteshit.lua
@@ -0,0 +1,78 @@
+-- title:   game title
+-- author:  game developer, email, etc.
+-- desc:    short description
+-- site:    website link
+-- license: MIT License (change this to your license of choice)
+-- version: 0.1
+-- script:  lua
+
+tile_hex = 0x04000
+tile_nib = tile_hex * 2
+
+t=0
+x=96
+y=24
+
+function BOOT()
+	cls(13)
+	rect_x = 5
+	rect_y = 5
+	
+	for i=1, 8 do
+		for ii=1, 8 do
+			trace(tile_hex)
+			trace(tile_nib)
+			trace(rect_x..rect_y)
+			tile0_color = peek4(tile_nib)
+			trace(tile0_color)
+		 rect(rect_x, rect_y, 10, 10, tile0_color)
+			tile_nib = tile_nib + 1
+			rect_x = rect_x + 10
+		end
+		rect_y = rect_y + 10
+		rect_x = 5
+	end
+end
+
+function TIC()
+
+	if btn(0) then y=y-1 end
+	if btn(1) then y=y+1 end
+	if btn(2) then x=x-1 end
+	if btn(3) then x=x+1 end
+
+
+	spr(1+t%60//30*2,x,y,14,3,0,0,2,2)
+	print("HELLO WORLD!",84,84)
+	t=t+1
+end
+-- <TILES>
+-- 000:333333323eeeee2b3eeee2cb3eee2ccb3ee2cccb3e2ccccb32cccccb2bbbbbbb
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <TRACKS>
+-- 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+-- </TRACKS>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+
